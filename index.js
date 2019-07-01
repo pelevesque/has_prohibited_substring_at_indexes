e4e73f7f840750a8e646a54482b66d3bda6a5b49
Pierre-Emmanuel Lévesque <pierre.e.levesque@gmail.com>
2019-07-01 12:27:55 -0400

    cleaned up code

--- index.js
+++ index.js
@@ -13,20 +13,25 @@ module.exports = (str, prohibitedSubstrings, allowSubstringBleeding = false) => 
   if (prohibitedSubstrings.length === 0) return false
   if (prohibitedSubstrings.length > 0 && str === '') return false
   let hasProhibitedSubstring = false
-  for (let i = 0, len = prohibitedSubstrings.length; i < len; i++) {
+  for (let i = 0, len = prohibitedSubstrings.length; i < len; i++) { //
     const index = prohibitedSubstrings[i][0]
-    let substring = prohibitedSubstrings[i][1]
-    if (allowSubstringBleeding) {
-      const substringMaxLength = str.length - index
-      if (substring.length > substringMaxLength) {
-        substring = substring.substr(0, substringMaxLength)
+    let substrings = prohibitedSubstrings[i][1]
+    if (!Array.isArray(substrings)) substrings = [substrings]
+    for (let j = 0, len = substrings.length; j < len; j++) {
+      let substring = substrings[j]
+      if (allowSubstringBleeding) {
+        const substringMaxLength = str.length - index
+        if (substring.length > substringMaxLength) {
+          substring = substring.substr(0, substringMaxLength)
+        }
+      }
+      const target = str.substr(index, substring.length)
+      if (substring.localeCompare(target) === 0) {
+        hasProhibitedSubstring = true
+        break
       }
     }
-    const target = str.substr(index, substring.length)
-    if (substring.localeCompare(target) === 0) {
-      hasProhibitedSubstring = true
-      break
-    }
+    if (hasProhibitedSubstring) break
   }
   return hasProhibitedSubstring
 }
